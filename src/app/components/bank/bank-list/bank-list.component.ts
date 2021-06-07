6eaae679b640e4ace413b830b5939f6c63ba9396
Mamata4198 <mamata.bb@terralogic.com>
2021-06-07 17:25:06 +0530

    update

--- src/app/components/bank/bank-list/bank-list.component.ts
+++ src/app/components/bank/bank-list/bank-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
 import { GridOptions } from "ag-grid-community";
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,6 +10,12 @@ import { GridOptions } from "ag-grid-community";
   styleUrls: ['./bank-list.component.css']
 })
 export class BankListComponent implements OnInit {
+  bank(bank: any, index: (Bank: any, index: any) => void) {
+    throw new Error('Method not implemented.');
+  }
+  index(Bank: any, index: any) {
+    throw new Error('Method not implemented.');
+  }
    gridOptions: GridOptions;
   Bank:any = [];
   columnDefs = [
@@ -53,11 +60,28 @@ export class BankListComponent implements OnInit {
 
 },
 
+{ field: '_id', suppressSizeToFit: true , width:  100 , headerName: 'Edit',
+        cellRenderer: params => {
+          return `Edit`;
+        },
+        onCellClicked: params => {
+          this.router.navigateByUrl('/bank/edit-bank/');
+        }
+      },
+      { field: '_id', suppressSizeToFit: true , width:  100 , headerName: 'Delete',
+      cellRenderer: params => {
+        return `delete`;
+      },
+      onCellClicked: params => {
+        this.removeBank(this.bank,this.index);
+        
+      }
+    }
 
 
 ];
    rowData : any;
-  constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService, private router : Router) {
     // this.readBank();
     // this.gridOptions = <GridOptions>{
      
